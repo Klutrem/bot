0c055b56bddc310c35c4b041f095ce2a7194c8cb
Klutrem <53902318+Klutrem@users.noreply.github.com>
2021-12-02 22:33:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def write_msg(user_id, message, keyboard):
 	vk.method('messages.send', {'user_id':user_id, 'message':message, 'random_id':time.time(), 'keyboard' : keyboard })
 
 
-token = 'f7d4e368b3687418d31cd63ca99a784964e527b633c079c62c6fff29594bbb23dba2f0720d1ce547d82be'
+token = ''
 vk = vk_api.VkApi(token = token)
 longpoll = VkLongPoll(vk)
 my_id = 62310117
